9338bd6882b7e7d8a56aa8b8a4155c6469270735
rim109 <132591560+rim109@users.noreply.github.com>
2023-12-07 14:57:22 +0900

    Add files via upload

---
+++ Calculator level 4.kt
@@ -0,0 +1,95 @@
+package com.example.myapplication
+fun main() {
+    var cal = Calculator()
+    println("식과 연산자를 차례대로 입력해주세요")
+    println("num1를 입력하시오")
+    var num1 = readln().toInt()
+
+    println("연산자를 입력해주세요")
+    var operator = readln().toString()
+
+    println("num2를 입력하시오")
+    var num2 = readln().toInt()
+
+
+    if(operator == "+") {
+        println(cal.allcal(AddOperation(),num1,num2))
+    } else if(operator == "-") {
+        // 뺄셈일 때
+        println(cal.allcal(MinusOperation(),num1,num2))
+    } else if(operator == "*") {
+        // 곱셈일 때
+        println(cal.allcal(MultipleOperation(),num1,num2))
+    } else if(operator == "/") {
+        // 나눗셈일 때
+        println(cal.allcal(DivideOperation(),num1, num2))
+    } else if (operator == "%"){
+        //나머지 연산자일 때
+        println(cal.allcal(ReminderOperation(),num1, num2))
+    } else {
+        // 잘못된 연산자 일 때
+        println("연산자가 잘못되어 실행되지 않습니다")
+
+    }
+
+}
+class Calculator {
+    fun allcal (caAddOperation: AbstractOperation, num1:Int, num2: Int): Double {
+        return caAddOperation.operation(num1,num2)
+    }
+//    fun caAdd (caAddOperation: AddOperation, num1:Int, num2: Int): Double {
+//        return caAddOperation.(num1,num2)
+//    }
+//
+//    fun caMinus(caMinusOperation: MinusOperation, num1: Int, num2: Int): Double{
+//        return caMinusOperation.minus(num1,num2)
+//    }
+//
+//    fun caMultiple(caMultipleOperation: MultipleOperation, num1: Int, num2: Int): Double{
+//        return caMultipleOperation.multiple(num1,num2)
+//    }
+//
+//    fun caDivide(caDivideOperation: DivideOperation, num1: Int, num2: Int): Double{
+//        return caDivideOperation.divide(num1,num2)
+//    }
+//
+//    fun caReminder(caReminderOperation: ReminderOperation, num1: Int, num2: Int): Double{
+//        return caReminderOperation.remainder(num1,num2)
+//    }
+
+}
+
+
+class AddOperation : AbstractOperation(){
+    override fun operation(num1: Int, num2: Int): Double {
+        return (num1 + num2).toDouble()
+    }
+}
+
+class MinusOperation: AbstractOperation(){
+    override fun operation(num1: Int, num2: Int): Double {
+        return (num1 - num2).toDouble()
+    }
+}
+
+class MultipleOperation: AbstractOperation(){
+    override fun operation(num1: Int, num2: Int): Double {
+        return (num1 * num2).toDouble()
+    }
+}
+
+class DivideOperation : AbstractOperation(){
+    override fun operation(num1: Int, num2: Int): Double {
+        return (num1 / num2).toDouble()
+    }
+}
+
+class ReminderOperation : AbstractOperation(){
+    override fun operation(num1: Int,num2: Int): Double {
+        return (num1 % num2).toDouble()
+    }
+}
+
+abstract class AbstractOperation {
+    abstract fun operation(num1: Int, num2: Int): Double
+}
